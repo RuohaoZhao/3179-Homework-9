f8f03297637a38746fce28485239e389f39a87cc
RuohaoZhao <91322072+RuohaoZhao@users.noreply.github.com>
2021-09-26 13:16:39 +0800

    Delete covid19.js

--- covid19.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "Covid19 - HM9.json"; 
-vegaEmbed("#map", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-    }).catch(console.error);
